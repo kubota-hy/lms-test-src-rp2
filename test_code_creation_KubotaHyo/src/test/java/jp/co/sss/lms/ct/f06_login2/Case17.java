cb147ae4747af64746de564844c714752204248f
vivid <vivid@namunamu>
2025-11-28 13:45:15 +0900

    add:Case17のテスト仕様書、テストコードの追加
    ・正常なパスワードを入力し、正常に変更がされる

--- test_code_creation_KubotaHyo/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
+++ test_code_creation_KubotaHyo/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能②
@@ -36,6 +45,11 @@ public class Case17 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		getEvidence(new Object() {},"test01");
+		String pageTitle = WebDriverUtils.webDriver.getTitle();
+		assertEquals("ログイン | LMS", pageTitle);
+
 	}
 
 	@Test
@@ -43,6 +57,18 @@ public class Case17 {
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA07");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA07");
+		webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']")).click();
+
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//h2[contains(text(),'利用規約')]")
+				));
+
+		getEvidence(new Object() {},"test02");
+
+		assertTrue(webDriver.getTitle().contains("セキュリティ規約"));
 	}
 
 	@Test
@@ -50,13 +76,43 @@ public class Case17 {
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
 		// TODO ここに追加
+
+		WebElement checkBox = webDriver.findElement(By.cssSelector("input[type='checkbox'][name='securityFlg']"));
+		WebElement nextBtn = webDriver.findElement(By.cssSelector("button.btn.btn-primary"));
+
+		checkBox.click();
+		nextBtn.click();
+
+		getEvidence(new Object() {},"test03");
+
+		assertTrue(webDriver.getTitle().contains("パスワード変更"));
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 変更パスワードを入力し「変更」ボタン押下")
-	void test04() {
+	void test04() throws InterruptedException {
 		// TODO ここに追加
+		
+		WebElement currentPassword = webDriver.findElement(By.id("currentPassword"));
+		WebElement newPassword = webDriver.findElement(By.id("password"));
+		WebElement passwordConfirm = webDriver.findElement(By.id("passwordConfirm"));
+		WebElement passwordChangeButton = webDriver.findElement(By.xpath("//button[@type='submit']"));
+
+		currentPassword.sendKeys("StudentAA07");
+		newPassword.sendKeys("LoginPass07");
+		passwordConfirm.sendKeys("LoginPass07");
+		passwordChangeButton.click();
+		
+		visibilityTimeout(By.xpath("//button[@id='upd-btn']"), 2);
+		WebElement changeconfirmDialog = webDriver.findElement(By.xpath("//button[@id='upd-btn']"));
+		changeconfirmDialog.click();
+
+		Thread.sleep(2000);
+		
+		getEvidence(new Object() {},"test04");
+		
+		assertTrue(webDriver.getTitle().contains("コース詳細"));
 	}
 
 }
